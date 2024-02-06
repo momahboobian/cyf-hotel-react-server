1bb6bb4d6cc1adfa5dcd44a41041010e7ca6d39d
Mo Mahboobian <moreelltd@gmail.com>
2024-02-06 11:28:42 +0000

    initial update

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const jsonEndpoints = require("./jsonEndpoints");
+const { testConnection, createTable, populateTable, hasRecords, db } = require("./databaseSetup");
+const dbEndpoints = require("./dbEndpoints");
+
+const app = express();
+const port = process.env.PORT || 5001;
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+
+testConnection()
+  .then(createTable)
+  .then(populateTable)
+  .catch((error) => {
+    console.error("Error setting up database:", error);
+  });
+  
+app.use("/videos", jsonEndpoints);
+app.use("/", dbEndpoints(db));
+
+const server = app.listen(port, () => {
+const { address, port } = server.address();
+const host = address === '::' ? 'localhost' : address;
+console.log(`Server is running at http://${host}:${port}`);
+});
